053c36f365bf06d2369dac07bea0e4217049d83c
jie <2538829529@qq.com>
2024-02-24 15:17:59 +0800

    feat:quad增加normal数据

--- src/object/quad.cpp
+++ src/object/quad.cpp
@@ -4,15 +4,15 @@ namespace MR {
 
 Quad::Quad() {
     float quad_vertices[] = {
-        0.5f,  0.5f,  0.0f, 1.0f, 1.0f,  // 右上角
-        0.5f,  -0.5f, 0.0f, 1.0f, 0.0f,  // 右下角
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,  // 左下角
-        -0.5f, 0.5f,  0.0f, 0.0f, 1.0f   // 左上角
+        0.5f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,  // 右上角
+        0.5f,  -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,  // 右下角
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,  // 左下角
+        -0.5f, 0.5f,  0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f   // 左上角
     };
-
+    // 逆时针
     unsigned int indices[] = {
-        0, 1, 3,  // 第一个三角形
-        1, 2, 3   // 第二个三角形
+        0, 3, 1,  // 第一个三角形
+        1, 3, 2   // 第二个三角形
     };
     // setup plane VAO
     unsigned int vbo, ebo;
@@ -30,12 +30,18 @@ Quad::Quad() {
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
                  GL_STATIC_DRAW);
 
+    // position
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
                           (void*)0);
-    glEnableVertexAttribArray(2);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+    // normal
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
                           (void*)(3 * sizeof(float)));
+    // uv
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                          (void*)(6 * sizeof(float)));
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
